afcfed199128df634c470c870d789454b4417025
Gaurav Porwal <gauravprwl14@gmail.com>
2022-09-10 17:33:36 +0530

    added the logout btn

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, useState } from "react";
-
+import { BiPowerOff } from 'react-icons/bi'
+import { signOut } from 'next-auth/react';
 
 interface Props {
 
@@ -7,8 +8,15 @@ interface Props {
 
 const SideBar: FunctionComponent<Props> = ({ }) => {
     return (
-        <div className="bg-red">
+        <div className="bg-red flex flex-1 h-full">
+            <div className="flex-1 h-full justify-center align-middle">
+                <div className="flex flex-1 h-8 justify-center align-middle" >
+                    <button className="flex-1 w-full" onClick={() => signOut()}>
+                        <BiPowerOff size={24} className="w-full" />
+                    </button>
 
+                </div>
+            </div>
         </div>
     )
 }
