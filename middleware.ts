040eced7bcfe3fbf1e5bf633219517fa7186135e
Gaurav Porwal <gauravprwl14@gmail.com>
2022-09-11 22:52:55 +0530

    refactor the database connection and added the mongoose package to connect to the database

--- middleware.ts
+++ middleware.ts
@@ -6,19 +6,55 @@ export async function middleware(req: NextRequest) {
     const autPage = [
         "/notes",
     ];
+    const isRootPath = pathname === '/'
     const conditionals = autPage.filter((path) => path == pathname)[0];
 
-    console.log('%c conditionals ', 'background: lime; color: black', { conditionals, pathname });
+    const url = req.nextUrl.clone();
+    const session = await getToken({
+        req,
+        // secret: process.env.JWT_SECRET,
+        secureCookie: process.env.NODE_ENV == "production",
+    });
+
+    console.log('%c conditionals ', 'background: lime; color: black', { conditionals, pathname, isRootPath });
+
+
+    if (session && isRootPath) {
+        url.pathname = '/notes'
+        return NextResponse.redirect(url);
+    }
 
     if (conditionals) {
-        const session = await getToken({
-            req,
-            secret: process.env.JWT_SECRET,
-            secureCookie: process.env.NODE_ENV == "production",
-        });
-
-        const url = req.nextUrl.clone();
         url.pathname = "/";
-        if (!session) return NextResponse.redirect(url);
+        if (!session) {
+            return NextResponse.redirect(url);
+        }
     }
+
+    return NextResponse.next()
+
+    // console.log('%c conditionals ', 'background: lime; color: black', { conditionals, pathname });
+
+    // if (conditionals) {
+    //     const session = await getToken({
+    //         req,
+    //         // secret: process.env.JWT_SECRET,
+    //         secureCookie: process.env.NODE_ENV == "production",
+    //     });
+
+    //     const url = req.nextUrl.clone();
+
+    //     if (session) {
+    //         if (isRootPath) {
+    //             url.pathname = "/notes";
+    //             return NextResponse.redirect(url);
+    //         } else {
+    //             return NextResponse.next()
+    //         }
+    //     } else {
+    //         url.pathname = "/";
+    //         return NextResponse.redirect(url);
+    //     }
+    // }
+    // return NextResponse.next()
 }
