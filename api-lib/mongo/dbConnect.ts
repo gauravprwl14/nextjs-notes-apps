a74ce837d683d7167c574628bf9629f7491bf5f8
Gaurav Porwal <gauravprwl14@gmail.com>
2022-09-11 22:51:56 +0530

    refactor the middleware file and added the logic of auth guard, redirecting the  user to login page if unauthorised, if logged in redirecting the user to notes page

--- api-lib/mongo/dbConnect.ts
+++ api-lib/mongo/dbConnect.ts
@@ -1,14 +1,16 @@
+// import { connectToDatabase } from '@/api-lib/mongo/dbConnect';
+import mongoose from "mongoose";
 import { MongoClient, Db } from "mongodb";
+import CONFIG from '@/api-lib/config'
 
+// const { MONGO_URL, MONGO_DB_NAME } = process.env
 
-const { MONGO_URL, MONGO_DB_NAME } = process.env
-
-const url = (MONGO_URL!) || "";
-let dbName = MONGO_DB_NAME;
+const url = (CONFIG.database.connection!) || "";
+let dbName = CONFIG.database.name;
 
 
 let cachedClient: MongoClient | null = null;
-let cachedDb: Db | null = null;
+let cachedDb: any = null;
 
 if (!url) {
     throw new Error(
@@ -22,14 +24,39 @@ if (!dbName) {
     );
 }
 
-export default async function connectToDatabase() {
-    // @ts-ignore
-    if (cachedClient && cachedDb) {
-        return { client: cachedClient, db: cachedDb };
+
+
+
+export const connectToMongoose = async () => {
+    try {
+        if (cachedDb) {
+            return {
+                db: cachedDb
+            }
+        }
+        const db = await mongoose.connect(url);
+        cachedDb = db;
+        console.log("Connected to mongodb.");
+        return {
+            db
+        }
+    } catch (error) {
+        console.error("Mongoose connection error.", error);
+        throw new Error("Unable to connect to mongodb.");
     }
-    const client = await MongoClient.connect(url);
-    const db = await client.db(dbName);
-    cachedClient = client;
-    cachedDb = db;
-    return { client, db };
-}
+};
+
+
+export default connectToMongoose
+
+// export default async function connectToDatabase() {
+//     // @ts-ignore
+//     if (cachedClient && cachedDb) {
+//         return { client: cachedClient, db: cachedDb };
+//     }
+//     const client = await MongoClient.connect(url);
+//     const db = await client.db(dbName);
+//     cachedClient = client;
+//     cachedDb = db;
+//     return { client, db };
+// }
