6d720d042cc0cb3c9ca824bc41cb47f75285b460
Gaurav Porwal <gauravprwl14@gmail.com>
2022-09-17 13:18:55 +0530

    added the react-context to handle the state of the application in the _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
+import { getSession, SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { AppProvider } from "../store/context";
+import { GetServerSidePropsContext } from 'next/types';
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <AppProvider notes={pageProps.pokemon}>
+        <Component {...pageProps} />
+      </AppProvider>
+    </SessionProvider>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
 
 export default MyApp
