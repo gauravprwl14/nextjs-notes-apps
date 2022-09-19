db4c1fd97adbcbf60972a0d3b08d57683348ffee
Gaurav Porwal <gauravprwl14@gmail.com>
2022-09-20 05:22:08 +0530

    generating the connection url for the mongodb by combining the base url and database name

--- api-lib/config/index.ts
+++ api-lib/config/index.ts
@@ -1,10 +1,11 @@
 const { MONGO_URL, MONGO_DB_NAME, NODE_ENV } = process.env
 import { MongoClientOptions } from "mongodb";
+import Logger from '@/api-lib/utils/logger'
 
 const CONFIG = {
 
     database: {
-        connection: (MONGO_URL! + MONGO_DB_NAME!) || "",
+        connection: (`${MONGO_URL}/${MONGO_DB_NAME}`) || "",
         name: MONGO_DB_NAME || "",
         options: {
             useUnifiedTopology: true,
@@ -13,5 +14,7 @@ const CONFIG = {
     },
     nodeEnv: NODE_ENV
 }
+
+Logger.debug('connectToMongoose', { database: CONFIG.database, ...CONFIG.database })
 
 export default CONFIG
