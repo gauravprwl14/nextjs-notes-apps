697b87e849c7200bb5db5358eb185d97d845da08
Gaurav Porwal <gauravprwl14@gmail.com>
2022-09-20 05:29:50 +0530

    added the generic fn to manage the api response

---
+++ api-lib/utils/response.tsx
@@ -0,0 +1,47 @@
+import type { NextApiResponse } from 'next'
+import { IResponsePayload } from '@/types/api'
+
+export const errorResponse = <T,>(payload: T, status: number) => {
+    return {
+        data: {
+            ...payload,
+
+        },
+        status,
+        isError: true
+    }
+}
+
+export const successResponse = <T,>(payload: T, status: number) => {
+    return {
+        data: {
+            ...payload,
+
+        },
+        status,
+        isError: false
+    }
+}
+
+export const responseHandler = <T = String>(res: NextApiResponse, payload: T, status: number, isError = false) => {
+
+    let apiResponse: IResponsePayload<T> = {
+        isError: false,
+        status: 200,
+        data: payload
+    }
+
+
+    const hasError = isError || status >= 400
+
+    if (hasError) {
+        apiResponse = errorResponse(payload, status)
+    } else {
+        apiResponse = successResponse(payload, status)
+    }
+
+
+    return res.status(status).json(apiResponse)
+
+
+}
