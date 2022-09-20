2dcfa183d68585b7be0bac995e1369353ff3931f
Gaurav Porwal <gauravprwl14@gmail.com>
2022-09-20 05:32:31 +0530

    added the types for the note collection

--- types/note.d.ts
+++ types/note.d.ts
@@ -1,43 +1,78 @@
-export type DeleteNoteBodyParams = {
-    multiple: boolean,
-    noteId?: number
+export interface INote {
+    _id: number;
+    note: Descendant[] | [];
+    createdAt: any;
 }
 
-export type UpdateNoteBodyParams = {
-    noteId: number,
-    idDoneVal: boolean,
+export interface IConnection {
+    _id: number;
+    name: String;
+    uid: number;
+    personnel_note: INote,
+    meeting_notes: INote[]
+}
+export interface INotes {
+    _id: number;
+    uid: number;
+    personnel_note: INote,
+    connections: IConnection[]
 }
 
-export type CreateNoteBodyParams = {
-    note: {
-        task: string,
-        isDone: boolean
-    },
-}
 
-export interface NoteItem {
-    task: string,
-    isDone: boolean
-}
 
-export interface SavedNoteItem extends NoteItem {
-    id: number
-}
 
-type deleteNote = ({ id: number }) => void;
-type toggleNote = ({ id: number, isDone: boolean }) => void
-type clearAllCompleted = () => void
-export interface NoteItemComponentProps extends SavedNoteItem {
-    deleteNote: deleteNote,
-    toggleNote: toggleNote,
-}
 
-export interface NoteListComponentProps {
-    loading: boolean,
-    noteList: SavedNoteItem[],
-    deleteNote: deleteNote,
-    toggleNote: toggleNote,
-    clearAllCompleted: clearAllCompleted,
-}
 
-export type FilterTab = "all" | "active" | "completed"
+
+
+
+
+
+
+
+
+
+
+// export type DeleteNoteBodyParams = {
+//     multiple: boolean,
+//     noteId?: number
+// }
+
+// export type UpdateNoteBodyParams = {
+//     noteId: number,
+//     idDoneVal: boolean,
+// }
+
+// export type CreateNoteBodyParams = {
+//     note: {
+//         task: string,
+//         isDone: boolean
+//     },
+// }
+
+// export interface NoteItem {
+//     task: string,
+//     isDone: boolean
+// }
+
+// export interface SavedNoteItem extends NoteItem {
+//     id: number
+// }
+
+// type deleteNote = ({ id: number }) => void;
+// type toggleNote = ({ id: number, isDone: boolean }) => void
+// type clearAllCompleted = () => void
+// export interface NoteItemComponentProps extends SavedNoteItem {
+//     deleteNote: deleteNote,
+//     toggleNote: toggleNote,
+// }
+
+// export interface NoteListComponentProps {
+//     loading: boolean,
+//     noteList: SavedNoteItem[],
+//     deleteNote: deleteNote,
+//     toggleNote: toggleNote,
+//     clearAllCompleted: clearAllCompleted,
+// }
+
+// export type FilterTab = "all" | "active" | "completed"
