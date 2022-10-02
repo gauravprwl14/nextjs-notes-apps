26e8caaf54d0ab688ad8261b0afae2819843a63e
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-02 16:07:40 +0530

    added the mutation query to store the notes in the backend by making an api call

--- store/notes.tsx
+++ store/notes.tsx
@@ -3,36 +3,66 @@ import { useState } from "react";
 import { getAPIContext, CONSTANTS } from 'utils/helper';
 import { fetcher, HttpMethods } from '../utils/fetcher'
 
-import { INotes } from '@/types/note'
+import { INote, INotes } from '@/types/note'
+import { getNotesListAPICall, postNoteAPICall } from 'services/note';
+import { dehydrate, DehydratedState, QueryClient, useMutation, useQueryClient } from '@tanstack/react-query';
 
 
+export const CONNECTION_ID = '631c6d4b2e19c822dd05c8c2'
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
 
-    const { baseUrl, headers } = getAPIContext(context)
-
-    let users: any = []
-    let notes = []
-
+export async function getServerSideProps(context: GetServerSidePropsContext): Promise<{ props: { dehydratedState: DehydratedState | null } }> {
     try {
+        const queryClient = new QueryClient()
 
-        notes = await fetcher(`${baseUrl}/api/notes/get`, HttpMethods.GET, headers, undefined)
-        if (notes) {
-            return notes
-        } else {
-            notes = []
+
+        await queryClient.prefetchQuery(['notes'], () => getNotesListAPICall(CONNECTION_ID));
+
+        return {
+            props: {
+                dehydratedState: dehydrate(queryClient)
+            }
         }
-
     } catch (error) {
-
+        return {
+            props: {
+                dehydratedState: null
+            }
+        }
     }
 
-    return {
-        props: {
-            notes: notes,
-            user: users
-        },
-    };
+
+
+    // const { baseUrl, headers } = getAPIContext(context)
+
+    // let users: any = []
+    // let notes: INote[] = []
+
+    // try {
+
+    //     notes = await fetcher(`${baseUrl}/api/notes/get?cid=${CONNECTION_ID}`, HttpMethods.GET, headers, undefined)
+    //     // notes = await getNotesListAPICall(CONNECTION_ID)
+
+    //     console.log('%c notes inside  getServerSideProps', 'background: lime; color: black', { notes });
+
+
+    //     if (notes) {
+    //         return notes
+    //     } else {
+    //         notes = []
+    //     }
+
+    // } catch (error) {
+    //     console.log('%c error inside notes getServerSideProps', 'background: salmon; color: black', { error });
+    // }
+
+    // return {
+    //     props: {
+    //         notes: notes,
+    //         user: users
+    //     },
+    // };
+
 
 }
 
@@ -77,14 +107,27 @@ let initialNote = [
 export const useNotesController = (notes: INotes[]) => {
     const [note, setNotes] = useState(initialNote);
 
+    const queryClient = useQueryClient()
+
+    // Mutations
+    const mutation = useMutation(postNoteAPICall, {
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries(['notes'])
+        },
+    })
+
     const handleBtnClick = async (payload: any) => {
-        console.log('%c window.c ', 'background: lime; color: black');
+        console.log('%c window.c ', 'background: lime; color: black', { payload });
 
         const headers = {
             credentials: 'include'
         }
         try {
-            const notes = await fetcher(`${CONSTANTS.baseUrl}/api/notes/create`, HttpMethods.POST, headers, { data: { note: payload, cid: '631c6d4b2e19c822dd05c8c2' } })
+            const requestPayload = { data: { note: payload, cid: CONNECTION_ID } }
+
+            mutation.mutate(requestPayload)
+            // const notes = await fetcher(`${CONSTANTS.baseUrl}/api/notes/create`, HttpMethods.POST, headers, requestPayload)
             console.log('%c notes handleBtnClick', 'background: lime; color: black', { notes });
         } catch (error) {
             console.log('%c error inside handleBtnClick notes ', 'background: salmon; color: black');
