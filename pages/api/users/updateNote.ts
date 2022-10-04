aa0856a195772e2bfb0802587367e42c8b3953af
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-04 19:03:54 +0530

    added the api end point to update the user notes

---
+++ pages/api/users/updateNote.ts
@@ -0,0 +1,84 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import NotesModel from '@/api-lib/model/Notes'
+import mongoose from 'mongoose';
+import { authInitializer } from '@/api-lib/utils/auth';
+import { responseHandler } from '@/api-lib/utils/response';
+import { z } from "zod";
+import Logger from '@/api-lib/utils/logger';
+import { generateResponseForPostNoteApi, serializeNote } from '@/api-lib/utils/helper';
+// import { getNotesList } from './get'
+
+
+const updateUserNoteValidator = z.object({
+    data: z.object({
+        note: z.any()
+    })
+})
+
+const update = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method !== 'PUT') {
+        const payload = { message: 'Method not allowed' }
+        return responseHandler(res, payload, 405)
+
+    }
+
+    try {
+        const { db, token, error, isError } = await authInitializer(req, res);
+        if (isError) {
+            responseHandler(res, error?.payload || "", error?.status || 500)
+        }
+
+        const userId = token?.uid as string
+
+        try {
+            updateUserNoteValidator.parse(req.body)
+        } catch (error) {
+            const payload = {
+                message: error
+            }
+            responseHandler(res, payload, 400)
+            return
+        }
+
+        const { note } = req.body?.data
+
+        const plainText = serializeNote(note)
+
+
+        const notesRecord = await NotesModel.findOneAndUpdate(
+            {
+                uid: userId
+            },
+            {
+                $set: {
+                    "personnelNote.note": note,
+                    "personnelNote.plainText": plainText
+                }
+            },
+            {
+                new: true,
+                runValidators: true,
+            }
+        )
+
+        const meetingNotes = generateResponseForPostNoteApi(notesRecord)
+
+        responseHandler(res, meetingNotes, 200)
+        return
+
+
+    } catch (error) {
+        Logger.error("update.user notes ", error)
+        let message = "Something went wrong" + error
+        if (error instanceof Error) {
+            message = error.message
+        }
+
+        responseHandler(res, { message: message }, 500)
+        return
+    }
+
+}
+
+export default update;
