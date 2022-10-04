3b0427ec1a869afdd60553d831444f1de600997a
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-04 18:28:13 +0530

    integrated the updated notes for the user connections

--- components/Common/SectionLayout/ProfileSection.tsx
+++ components/Common/SectionLayout/ProfileSection.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react'
+import { CONNECTION_ID, useConnectionController } from "@/store/connection";
+import { useQuery } from "@tanstack/react-query";
 import { FunctionComponent, useState } from "react";
 import { BiPencil } from 'react-icons/bi';
+import { getConnectionDetailsAPICall } from "services/connection";
+import Editor from './Editor'
+import { cloneDeep, initialEditorValue } from '@/store/notes';
 
 interface Props {
 
@@ -11,27 +17,24 @@ const ProfileIcon = () => {
     return <img className=" w-11 h-11 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar" />
 }
 
-export const Content = () => {
+
+interface ISectionLayoutProps {
+
+}
+
+
+export const Content = ({ note, handleSaveNotes, handleChange, isEditMode }: any) => {
+    let localNote = note ? note : cloneDeep(initialEditorValue)
+
     return (
-        <div className="mt-12 font-Inter font-medium text-base text-carbonGrey">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <div className="mt-2">
-
-                <ul className="pl-4 mt-7">
-                    {
-
-                        [1, 2, 3, 4, 5].map((item, index) => {
-                            return (
-                                <li key={index} className="list-disc mb-6">
-                                    Lorem Ipsum is simply dummy text
-                                </li>
-                            )
-                        })
-
-                    }
-                </ul>
-            </div>
-
+        <div className={`w-full  flex-wrap flex-1  px-6 rounded  mt-4 mb-5 ${isEditMode ? 'border-4 bg-white shadow-lg' : ''}`}>
+            <Editor
+                note={localNote}
+                handleChange={handleChange}
+                handleAddNote={handleSaveNotes}
+                btnText={'Save'}
+                isEditModeEnable={isEditMode}
+            />
         </div>
     )
 }
@@ -39,18 +42,55 @@ export const Content = () => {
 
 
 const ProfileSection: FunctionComponent<Props> = ({ }) => {
+    const { isEditMode, setEditMode, handleConnectionNoteUpdateBtnClick } = useConnectionController()
+    const [note, setNotes] = useState()
+
+    const handleChange = (newState: any) => {
+        setNotes(newState)
+    }
+
+    let { isLoading, data } = useQuery(['connectionDetails'], () =>
+        getConnectionDetailsAPICall(CONNECTION_ID)
+    );
+
+    useEffect(() => {
+        let note = data?.data?.personnelNote?.note
+        console.log('%c note inside the profile section', 'background: black; color: yellow', { note, data });
+        setNotes(note)
+
+    }, [data]
+    )
+
+
+    const handleSaveNotes = (localNote: any) => {
+        handleConnectionNoteUpdateBtnClick(localNote)
+    }
+
     return (
-        <div className="flex mt-3 flex-row justify-between items-center">
-            <div className=" flex flex-row justify-center items-center">
-                <ProfileIcon />
-                <div className=" ml-4 ">
-                    <span className="text-terraCotta font-Inter font-bold text-lg"> John  Smith </span>
+        <div>
+            <div className="flex mt-3 flex-row justify-between items-center">
+                <div className=" flex flex-row justify-center items-center">
+                    <ProfileIcon />
+                    <div className=" ml-4 ">
+                        <span className="text-terraCotta font-Inter font-bold text-lg"> John  Smith </span>
+                    </div>
+                </div>
+
+                <div>
+                    {
+                        !isEditMode && <div onClick={() => {
+                            setEditMode(true)
+                        }}><BiPencil /></div>
+                    }
+
                 </div>
             </div>
-
-            <div>
-                <BiPencil />
-            </div>
+            <Content
+                note={note}
+                handleSaveNotes={handleSaveNotes}
+                handleChange={handleChange}
+                isEditMode={isEditMode}
+            />
         </div>
     )
 }
