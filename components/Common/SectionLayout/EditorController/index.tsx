991ba5cb0b2c964e5dac89c1aa01ebe9d0d4d5d2
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-04 19:06:16 +0530

    added the editor controller

---
+++ components/Common/SectionLayout/EditorController/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Editor from '../Editor'
+import { cloneDeep, initialEditorValue } from '@/store/notes';
+import { ISlateNote } from '@/types/note';
+
+interface IEditorControllerProps {
+    note: ISlateNote | null | undefined
+    handleSaveNotes: (...args: any) => void
+    handleChange: (...args: any) => void
+    isEditMode: boolean
+}
+
+
+
+export const EditorController = ({ note, handleSaveNotes, handleChange, isEditMode }: IEditorControllerProps) => {
+    let localNote = note ? note : cloneDeep(initialEditorValue)
+
+    return (
+        <div className={`w-full  flex-wrap flex-1  px-6 rounded  mt-4 mb-5 ${isEditMode ? 'border-4 bg-white shadow-lg' : ''}`}>
+            <Editor
+                note={localNote}
+                handleChange={handleChange}
+                handleAddNote={handleSaveNotes}
+                btnText={'Save'}
+                isEditModeEnable={isEditMode}
+            />
+        </div>
+    )
+}
+
+export default EditorController
