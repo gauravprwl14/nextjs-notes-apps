01ca06e32fd44aa26ec683ca592d66a2dc3698a4
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-05 15:31:09 +0530

    added the page to show user

--- components/Page/SectionLayout/index.tsx
+++ components/Page/SectionLayout/index.tsx
@@ -37,10 +37,10 @@ const SectionLayout: FunctionComponent<ISectionLayoutProps> = ({ }) => {
     return (
         <div className="flex flex-row w-full h-full">
 
-            <div className="flex-1 w-[25%] bg-whiteSmoke h-full pt-3 px-7">
+            <div className="w-[25%] bg-whiteSmoke h-full pt-3 px-7">
                 <ProfileSection />
             </div>
-            <div className="flex-1 w-[50%] flex-wrap overflow-auto pt-3 px-7 relative">
+            <div className="w-[50%] flex-wrap overflow-auto pt-3 px-7 relative">
                 <div className="w-full shadow-lg flex-wrap flex-1 border-4 px-6 rounded bg-white mb-5">
                     <Editor
                         note={note}
@@ -54,7 +54,7 @@ const SectionLayout: FunctionComponent<ISectionLayoutProps> = ({ }) => {
                     onDeleteClick={handleDeleteBtnClick}
                 />
             </div>
-            <div className="flex-1 w-[25%] border-l-2 pl-6"> <MyNotes /></div>
+            <div className="w-[25%] border-l-2 pl-6"> <MyNotes /></div>
 
         </div>
     )
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,19 +1,52 @@
+
 import { FunctionComponent, useState } from "react";
 import { BiPowerOff } from 'react-icons/bi'
+import { FaUserPlus, FaBookOpen } from 'react-icons/fa'
 import { signOut } from 'next-auth/react';
+import Link from "next/link";
 
 interface Props {
 
+}
+
+
+interface ISideBarButtonProps {
+    children: React.ReactNode;
+    btnClick?: () => void
+}
+
+const SideBarButton = ({ children, btnClick, ...props }: ISideBarButtonProps) => {
+    return (
+        <button className="flex-1 w-full my-2 " onClick={btnClick} {...props}>
+            {children}
+        </button>
+
+    )
 }
 
 const SideBar: FunctionComponent<Props> = ({ }) => {
     return (
         <div className="bg-red flex flex-1 h-full">
             <div className="flex-1 h-full justify-center align-middle">
-                <div className="flex flex-1 h-8 justify-center align-middle" >
-                    <button className="flex-1 w-full" onClick={() => signOut()}>
+                <div className="flex flex-1 justify-center align-middle flex-col " >
+
+                    <Link href="/users">
+                        <SideBarButton>
+                            <FaUserPlus size={20} className="w-full" />
+                        </SideBarButton>
+                    </Link>
+
+
+                    <Link href="/notes">
+                        <SideBarButton>
+                            <FaBookOpen size={20} className="w-full" />
+                        </SideBarButton>
+                    </Link>
+
+
+                    <SideBarButton btnClick={() => signOut()}>
                         <BiPowerOff size={24} className="w-full" />
-                    </button>
+                    </SideBarButton>
 
                 </div>
             </div>
