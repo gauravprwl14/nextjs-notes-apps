bc03bed67849c85bb7fc2042142961287c9c34e7
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-08 13:10:37 +0530

    added the input field

---
+++ components/Input/index.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, FocusEvent } from "react";
+
+type InputProps = {
+    placeholder: string;
+    label: string;
+    className?: string;
+    defaultValue?: string;
+    error?: string;
+    id?: string;
+    onBlur?: (event: FocusEvent<HTMLInputElement>) => void;
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+    type: string;
+    disabled?: boolean;
+    value?: string;
+};
+
+export const Input = ({
+    placeholder,
+    label,
+    className,
+    defaultValue,
+    error,
+    onBlur,
+    id,
+    onChange,
+    type,
+    disabled,
+    value,
+}: InputProps) => {
+    return (
+        <div className="w-full">
+            <label className="flex flex-col label">
+                {label}
+                <input
+                    className={`input placeholder:base ${className}`}
+                    type={type}
+                    placeholder={placeholder}
+                    defaultValue={defaultValue}
+                    onBlur={onBlur}
+                    id={id}
+                    onChange={onChange}
+                    disabled={disabled}
+                    value={value}
+                />
+            </label>
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+        </div>
+    );
+};
+
+export default Input
