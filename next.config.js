983e152f4bc6db0df38fa03b57bdf0739b55779a
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-09 10:59:22 +0530

    added the config for external source of image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        domains: ['flowbite.com'],
+    },
 }
 
 module.exports = nextConfig
