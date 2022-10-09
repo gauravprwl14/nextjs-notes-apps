923ae9f5bd4caf3ae02b12aa5b157b07947d7285
Gaurav Porwal <gauravprwl14@gmail.com>
2022-10-09 12:16:32 +0530

    passing the connection object as a props from parent to children

--- components/Page/SectionLayout/index.tsx
+++ components/Page/SectionLayout/index.tsx
@@ -6,6 +6,7 @@ import MyNotes from './MyNotes'
 
 import { useNotesController } from '@/store/notes';
 import { INote } from "@/types/note";
+import { useSelectConnectionController } from "@/store/connection";
 
 interface ISectionLayoutProps {
 
@@ -15,6 +16,13 @@ interface ISectionLayoutProps {
 const SectionLayout: FunctionComponent<ISectionLayoutProps> = ({ }) => {
     const { setNotes, note, handleBtnClick, handleNoteEdit, handleNoteDelete } = useNotesController([])
 
+    const {
+        selectConnection,
+        setSelectedConnection,
+        connectionList,
+        isConnectionListLoading
+    } = useSelectConnectionController()
+
     const handleChange = (newState: any) => {
         setNotes(newState)
     }
@@ -22,8 +30,13 @@ const SectionLayout: FunctionComponent<ISectionLayoutProps> = ({ }) => {
     const handleAddNotes = (localNote: any) => {
         // e.preventDefault()
 
+        // todo handle the error condition,show a toast msg or disable the btn if connection obj is not present
+        if (selectConnection?._id) {
+            handleBtnClick(localNote, selectConnection._id)
+        }
 
-        handleBtnClick(localNote)
+
+
     }
     const handleEditBtnClick = (payload: { noteObj: INote }) => {
 
@@ -38,7 +51,12 @@ const SectionLayout: FunctionComponent<ISectionLayoutProps> = ({ }) => {
         <div className="flex flex-row w-full h-full">
 
             <div className="w-[25%] bg-whiteSmoke h-full pt-3 px-7">
-                <ProfileSection />
+                <ProfileSection
+                    selectConnection={selectConnection}
+                    setSelectedConnection={setSelectedConnection}
+                    connectionList={connectionList}
+                    isConnectionListLoading={isConnectionListLoading}
+                />
             </div>
             <div className="w-[50%] flex-wrap overflow-auto pt-3 px-7 relative">
                 <div className="w-full shadow-lg flex-wrap flex-1 border-4 px-6 rounded bg-white mb-5">
@@ -52,6 +70,7 @@ const SectionLayout: FunctionComponent<ISectionLayoutProps> = ({ }) => {
                 <NoteList
                     onEditBtnClick={handleEditBtnClick}
                     onDeleteClick={handleDeleteBtnClick}
+                    cObj={selectConnection}
                 />
             </div>
             <div className="w-[25%] border-l-2 pl-6"> <MyNotes /></div>
